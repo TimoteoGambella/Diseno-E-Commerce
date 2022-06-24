ddd4b1866b98bcedd00f35d41864ec20b967ca6b
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-06-24 19:11:24 +0200

    Home

--- components/HomeContainer.js
+++ components/HomeContainer.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Image from 'next/image';
-import background from '../public/img/modeloimportante.jpeg';
-import NavbarMenu from './NavbarMenu';
+
+import Link from 'next/link'
 
 const HomeContainer = () => {
     return (
         <div className='home-container'>
-            <div className='img'>
-                <NavbarMenu/>
+            <div className='home-nav'>
+                <Link href="/Nosotros">Nosotros</Link>
+                <Link href="/Tienda">Tienda</Link>
             </div>
         </div>
     );
