47b1c3df3aac947e46573ff45b933bf2cc034cf2
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-06-24 19:21:21 +0200

    Home

--- components/HomeContainer.js
+++ components/HomeContainer.js
@@ -5,6 +5,12 @@ import Link from 'next/link'
 const HomeContainer = () => {
     return (
         <div className='home-container'>
+
+            <div className='home-title'>
+                <h1>BENET</h1>
+            </div>
+
+
             <div className='home-nav'>
                 <Link href="/Nosotros">Nosotros</Link>
                 <Link href="/Tienda">Tienda</Link>
